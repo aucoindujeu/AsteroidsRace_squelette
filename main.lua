320707078eb10c81994fdb6022b3a729a294b075
Jehadel <jean.delpech@gmail.com>
2023-10-31 01:24:14 +0100

    Feat/Mode 2 joueureuses

--- main.lua
+++ main.lua
@@ -1,4 +1,13 @@
-require "classic" -- import bibliothèque rxi/classic pour POO sous license MIT (fichier license joint)
+-- TO DO
+-- * corriger bug collision
+-- * faire le menu de sélection multijoueur
+-- * gérer le positionnement des vaisseaux au départ en mode multijoueur (décentrés)
+-- * gérer l’affichage des scores/tentatives par joueur durant le jeu
+-- * gérer l’affichage des socres/tentatives par joueur sur écran game over
+
+
+
+Objet = require "classic" -- import bibliothèque rxi/classic pour POO sous license MIT (fichier license joint)
 
 -- **********************************
 -- Variables utilisées dans le jeu
@@ -19,13 +28,8 @@ PAUSE_RESSUSCITE = 10
 chrono = 0 
 direction = {-1, 1}
 etatJeu = 'demarrage'
+multijoueureuses = false
 
-joueur = {}
-joueur.score = 0
-joueur.tentatives = 0
-joueur.vitesse = VITESSE_JOUEUR
-joueur.touche = false
-joueur.pause = PAUSE_RESSUSCITE 
 asteroides = {}
 asteroides.liste = {}
 
@@ -33,14 +37,94 @@ asteroides.liste = {}
 -- Son et images (sprites)
 -- ***************
 
--- Sprite joueur
-joueur.image = love.graphics.newImage('joueur.png')
-joueur.imgExplosion = love.graphics.newImage('explosion.png')
-joueur.largeur = joueur.image:getWidth()
-joueur.hauteur = joueur.image:getHeight()
-joueur.sonMoteur = love.audio.newSource('moteur.wav', 'static')
-joueur.sonMoteur:setLooping(true)
-joueur.sonExplosion = love.audio.newSource('explosion.wav', 'static')
+-- Sprite joueur (objet)
+Joueureuse = Objet:extend()
+
+function Joueureuse:new(pImgJoueur, pKeyUp, pKeyDown)
+
+  self.image = love.graphics.newImage(pImgJoueur)
+  self.imgExplosion = love.graphics.newImage('explosion.png')
+  self.largeur = self.image:getWidth()
+  self.hauteur = self.image:getHeight()
+  self.sonMoteur = love.audio.newSource('moteur.wav', 'static')
+  self.sonMoteur:setLooping(true)
+  self.sonExplosion = love.audio.newSource('explosion.wav', 'static')
+  
+  self.x = (LARGEUR_ECRAN - self.largeur)/2
+  self.y = HAUTEUR_ECRAN - self.hauteur - HAUTEUR_INFO
+
+  self.score = 0
+  self.tentatives = 0
+  self.vitesse = VITESSE_JOUEUR
+
+  self.up = pKeyUp
+  self.down = pKeyDown
+
+  self.touche = false
+  self.pause = PAUSE_RESSUSCITE 
+
+end
+
+
+function Joueureuse:update(dt)
+
+  if self.touche == false then -- si le vaisseau n’est pas touché : il peut bouger
+    if love.keyboard.isDown(self.up) then
+      self.y = self.y - dt * self.vitesse
+      self.sonMoteur:setPitch(2)
+        
+    elseif love.keyboard.isDown(self.down) and (self.y < HAUTEUR_ECRAN - self.hauteur - HAUTEUR_INFO) then
+      self.y = self.y + dt * self.vitesse
+      self.sonMoteur:setPitch(0.5)
+
+    else
+      self.sonMoteur:setPitch(1)
+    
+    end
+
+  else 
+    self.pause = self.pause - dt -- petite pause quand le vaisseau explose
+    if self.pause <= 0 then
+      self.y = HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO -- pause finie : on replace le vaisseau en bas
+      self.touche = false -- on réinitialise
+      self.pause = PAUSE_RESSUSCITE
+    end
+  end
+
+    -- on replace le vaisseau si d’avenrure il est descendu trop bas
+    if self.y > HAUTEUR_ECRAN - self.hauteur - HAUTEUR_INFO then
+      self.y = HAUTEUR_ECRAN - self.hauteur - HAUTEUR_INFO
+    end
+    
+    -- si le vaisseau a atteint le haut de l’écran on le renvoie en bas et m-à-j. score
+    if (self.y + self.hauteur < 0) then
+      self.score = self.score + 1
+      self.y = HAUTEUR_ECRAN - self.hauteur - HAUTEUR_INFO 
+    end
+
+end
+
+
+function Joueureuse:draw()
+
+    if self.touche == false then
+      love.graphics.draw(self.image, self.x, self.y)
+    else
+      love.graphics.draw(self.imgExplosion, self.x, self.y)
+    end
+
+end
+
+
+function Joueureuse:collision()
+  
+  self.tentatives = self.tentatives + 1
+  -- On gère l’explosion (+ sauvegarder dernière position joueur)
+  self.touche = true
+  self.sonExplosion:play()
+
+end
+   
 
 -- Sprite étoiles
 asteroides.image = love.graphics.newImage('asteroide.png')
@@ -65,17 +149,25 @@ function initJeu()
   
   chrono = TEMPS_DE_JEU
   etatJeu = 'en jeu'
-  joueur.x = (LARGEUR_ECRAN - joueur.largeur)/2
-  joueur.y = HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO
-  joueur.sonMoteur:play()
-  joueur.score = 0
+  
+  lstJoueureuses = {}
+
+  table.insert(lstJoueureuses, Joueureuse('joueureuse1.png', 'up', 'down'))
+
+  if multijoueureuses == true then
+    table.insert(Joueureuses('joueureuse2.png', 'z', 's'))
+  end
+
+  for k, j in ipairs(lstJoueureuses) do
+    j.sonMoteur:play()
+  end
 
   -- Création des étoiles
   asteroides.liste = {} -- on vide la liste de sprites asteroides
   for n=1, NOMBRE_ASTEROIDES do
     local asteroide = {}
     asteroide.x = love.math.random(1, LARGEUR_ECRAN)
-    asteroide.y = love.math.random(1, HAUTEUR_ECRAN - 2*joueur.hauteur - HAUTEUR_INFO)
+    asteroide.y = love.math.random(1, HAUTEUR_ECRAN - 2*lstJoueureuses[1].hauteur - HAUTEUR_INFO)
     asteroide.direction = direction[love.math.random(1,2)]
     asteroide.vitesse = VITESSE_ASTEROIDES - love.math.random(VARIO_ASTEROIDES)
 
@@ -87,7 +179,7 @@ end
 
 
 function love.load()
-
+  
   love.window.setMode(LARGEUR_ECRAN, HAUTEUR_ECRAN)
   love.window.setTitle('Asteroids Race - Code Club CimeLab - Au Coin du jeu')
   love.graphics.setFont(police)
@@ -96,12 +188,11 @@ end
 
 function love.update(dt)
   
-
   if etatJeu == 'demarrage' then
 
     -- selection mode 1 ou 2 joueur
     --
-   --
+    --
   elseif etatJeu == 'en jeu' then
     
     --*******************
@@ -114,30 +205,11 @@ function love.update(dt)
     end
 
     --*****************
-    -- UPDATE JOUEUR
+    -- UPDATE JOUEUREUSES
     --*****************
     
-    if (joueur.y + joueur.hauteur < 0) then
-      joueur.score = joueur.score + 1
-      joueur.y = HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO 
-    end
-    if joueur.touche == false then 
-      if love.keyboard.isDown('up') then
-        joueur.y = joueur.y - dt*joueur.vitesse
-        joueur.sonMoteur:setPitch(2)
-        
-      elseif love.keyboard.isDown('down') and (joueur.y < HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO) then
-        joueur.y = joueur.y + dt*joueur.vitesse
-        joueur.sonMoteur:setPitch(0.5)
-
-      else
-        joueur.sonMoteur:setPitch(1)
-      end
-    end
-
-    -- on replace le joueur si d’avenure il est descendu trop bas
-    if joueur.y > HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO then
-      joueur.y = HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO
+    for k, j in ipairs(lstJoueureuses) do
+      j:update(dt)
     end
 
     --**********************
@@ -154,26 +226,19 @@ function love.update(dt)
       end
    
       -- teste collision de chaque étoile avec la fusée
-      if testeCollision(asteroide.x, asteroide.y, asteroides.largeur, asteroides.hauteur, joueur.x, joueur.y, joueur.largeur, joueur.hauteur) and joueur.touche == false then
-        joueur.tentatives = joueur.tentatives + 1
-        -- On gère l’explosion (+ sauvegarder dernière position joueur)
-        joueur.touche = true
-        joueur.sonExplosion:play()
-      end
-      if joueur.touche == true then
-        joueur.pause = joueur.pause - dt
-        if joueur.pause <= 0 then
-          joueur.y = HAUTEUR_ECRAN - joueur.hauteur - HAUTEUR_INFO
-          joueur.touche = false
-          joueur.pause = PAUSE_RESSUSCITE
+      for k, j in ipairs(lstJoueureuses) do
+        if testeCollision(asteroide.x, asteroide.y, asteroides.largeur, asteroides.hauteur, j.x, j.y, j.largeur, j.hauteur) and j.touche == false then
+        --j.collision()
         end
-      end
-
+      end  
+    
     end
 
   elseif etatJeu == 'game over' then
 
-    joueur.sonMoteur:stop()
+    for k, j in ipairs(lstJoueureuses) do
+      j.sonMoteur:stop()
+    end
     
   else
     etatJeu = 'demarrage'
@@ -192,30 +257,37 @@ function love.draw()
     -- *****************
     -- AFFICHAGE INFO
     -- *****************
+    
     love.graphics.line(0, HAUTEUR_ECRAN - HAUTEUR_INFO, LARGEUR_ECRAN, HAUTEUR_ECRAN - HAUTEUR_INFO)
-    love.graphics.print("Score:"..tostring(joueur.score), 10, HAUTEUR_ECRAN - 45)
-    love.graphics.print("Tentatives:"..tostring(joueur.tentatives), 10, HAUTEUR_ECRAN -25)
     love.graphics.printf(math.ceil(chrono), 0, HAUTEUR_ECRAN - 45, LARGEUR_ECRAN, 'center')
 
-    -- *****************
-    -- AFFICHAGE JOUEUR
-    -- *****************
-    if joueur.touche == false then
-      love.graphics.draw(joueur.image, joueur.x, joueur.y)
-    else
-      love.graphics.draw(joueur.imgExplosion, joueur.x, joueur.y)
+    -- ***********************
+    -- AFFICHAGE JOUEUREUSES
+    -- **********************
+   
+    for k, j in ipairs(lstJoueureuses) do
+      j:draw()
+      love.graphics.print("Score:"..tostring(j.score), 10, HAUTEUR_ECRAN - 45)
+      love.graphics.print("Tentatives:"..tostring(j.tentatives), 10, HAUTEUR_ECRAN -25)
+
     end
-    -- *****************
-    -- AFFICHAGE asteroideS 
-    -- *****************
+
+    -- *********************
+    -- AFFICHAGE ASTEROIDES 
+    -- *********************
+    
     for index, asteroide in ipairs(asteroides.liste) do
       love.graphics.draw(asteroides.image, asteroide.x, asteroide.y)
     end
 
   elseif etatJeu == 'game over' then
     
-    love.graphics.printf("Score :"..tostring(joueur.score).." Tentatives :"..tostring(joueur.tentatives), 0, HAUTEUR_ECRAN/2, LARGEUR_ECRAN, 'center')
+    for k, j in ipairs(lstJoueureuses) do
+      love.graphics.printf("Score :"..tostring(j.score).." Tentatives :"..tostring(j.tentatives), 0, HAUTEUR_ECRAN/2, LARGEUR_ECRAN, 'center')
+    end
+    
     love.graphics.printf("Entrée pour revenir au menu", 0, HAUTEUR_ECRAN/2 + 30, LARGEUR_ECRAN, 'center')
+
   end
 end
 
